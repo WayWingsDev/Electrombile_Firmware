b9ad1512ad0204a9c07c9646141689c073460558
Jason <jeson.g@gmail.com>
2015-07-03 16:48:44 +0800

    fix typo

--- user/electrombile/log.h
+++ user/electrombile/log.h
@@ -12,19 +12,19 @@
 #define _ERROR
 
 #ifdef LOG_DEBUG_FLAG
-#define LOG_DEBUG(fmt, ...) eat_trace("[DBG][s]"fmt, __FUNCTION__, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) eat_trace("[DBG][%s]"fmt, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(fmt, ...)
 #endif
 
 #ifdef LOG_INFO_FLAG
-#define LOG_INFO(fmt, ...) eat_trace("[INF][s]"fmt, __FUNCTION__, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) eat_trace("[INF][%s]"fmt, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_INFO(fmt, ...)
 #endif
 
 #ifdef LOG_ERROR_FLAG
-#define LOG_ERROR(fmt, ...) eat_trace("[ERR][s]"fmt, __FUNCTION__, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) eat_trace("[ERR][%s]"fmt, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_ERROR(fmt, ...)
 #endif
