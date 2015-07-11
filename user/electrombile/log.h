02667e740226a4f871bd1bdf588e964dadc1f89c
Jason <jeson.g@gmail.com>
2015-07-11 16:03:19 +0800

    change separator - to space

--- user/electrombile/log.h
+++ user/electrombile/log.h
@@ -9,19 +9,19 @@
 #define USER_ELECTROMBILE_LOG_H_
 
 #ifdef LOG_DEBUG_FLAG
-#define LOG_DEBUG(fmt, ...) eat_trace("[DBG][%s:%d-%s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) eat_trace("[DBG][%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(fmt, ...)
 #endif
 
 #ifdef LOG_INFO_FLAG
-#define LOG_INFO(fmt, ...) eat_trace("[INF][%s:%d-%s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) eat_trace("[INF][%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_INFO(fmt, ...)
 #endif
 
 #ifdef LOG_ERROR_FLAG
-#define LOG_ERROR(fmt, ...) eat_trace("[ERR][%s:%d-%s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) eat_trace("[ERR][%s:%d %s]"fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
 #define LOG_ERROR(fmt, ...)
 #endif
